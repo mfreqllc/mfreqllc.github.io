9eaef445895b6014181249cf01680eec22bd5a4d
Matt Osmus <matt.osmus@gmail.com>
2025-04-21 11:18:19 -0500

    Initial commit.

---
+++ software/js/init.js
@@ -0,0 +1,13 @@
+_5grid.ready(function() {
+
+	if (_5grid.isDesktop)
+	{
+		$('#nav > ul').dropotron({ 
+			offsetY: -18,
+			mode: 'fade',
+			noOpenerFade: true,
+			hideDelay: 350
+		});
+	}
+
+});
